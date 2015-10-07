71cada3cc3c305e1965bcb0b1a0b5f9e508fec4c
Samin Silwal <smnslwl@gmail.com>
2015-10-07 11:51:42 +0545

    Fix minor formatting issue

--- 8/8.py
+++ 8/8.py
@@ -2,7 +2,7 @@
    The four adjacent digits in the 1000-digit number that have the greatest
    product are 9 × 9 × 8 × 9 = 5832.
 
-               {{8_input.txt}}
+                {{ 8_input.txt }}
 
    Find the thirteen adjacent digits in the 1000-digit number that have the
    greatest product. What is the value of this product?
